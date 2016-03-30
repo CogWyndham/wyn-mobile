ef9a26318cdaf8f5114cd789e13e46ff251500ac
satyaram <satyaram@NJITSATYARAMD6.WYN.WYNDHAMWORLDWIDE.COM>
2016-03-30 08:12:56 -0400

    narenra

---
+++ CRAFT_mCRAFT/testscripts/MobileTestingScenario/Scenario1.java
@@ -0,0 +1,46 @@
+package testscripts.MobileTestingScenario;
+import businesscomponents.*;
+import supportlibraries.ScriptHelper;
+public class Scenario1 extends LoopNewApplicant {
+
+	public Scenario1(ScriptHelper scriptHelper) {
+		super(scriptHelper);
+		// TODO Auto-generated constructor stub
+	}
+	
+	String resumeSource;
+	String stagingURL;
+	String jobKeyword;
+	String jobLocation;
+	String browserTitle;
+	
+	//String resumeSource = "DropBox";
+
+	//String stagingURL = "http://wynexternalstage.loop.jobs/";
+	// String stagingURL = "http://wynp2stage.loop.jobs/";
+
+	//String jobKeyword = "US REQUISITION - TEST AUTOMATION";
+	//String jobLocation = "Parsippany";
+
+	//String browserTitle = "Welcome - Apply Process";
+	
+
+	public void ScenarioExecution1() throws Exception {
+		try {
+		// TODO Auto-generated method stub
+
+			navigateToStagingURL(stagingURL);
+			searchAndApplyForRequisition(jobKeyword, jobLocation);
+			switchBrowserWindow(browserTitle);
+			
+			
+	} catch (Exception e) {
+		System.out.println("Scenario is not executed");
+		e.printStackTrace();
+	}finally {
+		testTearDown();
+	}
+	}
+}
+
+
